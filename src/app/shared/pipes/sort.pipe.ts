a609dc67ac5d3b9f4037a0ef7a3e2364f9262525
maxxym07 <maksymko18@gmail.com>
2020-07-31 13:22:59 +0300

    first commit

--- src/app/shared/pipes/sort.pipe.ts
+++ src/app/shared/pipes/sort.pipe.ts
@@ -7,35 +7,24 @@ import { ICategory } from '../interfaces/category.interface';
 export class SortPipe implements PipeTransform {
 
   transform(cat: Array<ICategory>, sortIDCol: boolean, sortENCol: boolean, sortUACol: boolean): unknown {
-    if (sortENCol == true) {
-      return cat.sort(function (a, b) {
-        if (a.nameEN.toLowerCase()< b.nameEN.toLowerCase()) {
-         return -1
-        }
-        
-      })
+ 
+    if (sortENCol==true) {
+      return cat.sort((a, b) => a.nameEN.toLowerCase() < b.nameEN.toLowerCase() ? -1 : 1);
     }
 
-    if (sortUACol == true) {
-      return cat.sort(function (a, b) {
-        if (a.nameUA.toLowerCase() < b.nameUA.toLowerCase()) {
-          return -1
-        }
-      })
+    else if (sortUACol==true) {
+      return cat.sort((a, b) => a.nameUA.toLowerCase() < b.nameUA.toLowerCase() ? -1 : 1);
     }
-
-    if (sortIDCol == true) {
-      return cat.sort(function (a, b) {
-        if (a.id < b.id) {
-          return -1
-        }
-      })
+      
+    else if (sortIDCol==true) {
+      return cat.sort((a, b) => a.id < b.id ? -1 : 1).reverse();
     }
-  
-    else {
-      return cat.reverse()
+      
+    else if (sortENCol === false || sortUACol === false || sortIDCol === false) {
+      return cat.reverse();
     }
-
+      
+    else return cat;
   }
 
-}
+  }
