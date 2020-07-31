b85b6af2a0010e4109d9b27ba4e7cc807d30d822
maxxym07 <maksymko18@gmail.com>
2020-07-31 12:16:08 +0300

    first commit

--- src/app/shared/pipes/sort.pipe.ts
+++ src/app/shared/pipes/sort.pipe.ts
@@ -7,19 +7,50 @@ import { ICategory } from '../interfaces/category.interface';
 export class SortPipe implements PipeTransform {
 
   transform(cat: Array<ICategory>, sortIDCol: boolean, sortENCol: boolean, sortUACol: boolean): unknown {
+
     if (sortENCol == true) {
-      return cat.sort((a, b) => a.nameEN.toLowerCase() < b.nameEN.toLowerCase() ? -1 : 1);
-    };
+      return cat.sort(function (a, b) {
+        if (a.nameEN.toLowerCase()< b.nameEN.toLowerCase()) {
+         return -1
+        }
+        
+      })
+    }
 
     if (sortUACol == true) {
-      return cat.sort((a, b) => a.nameUA.toLowerCase() < b.nameUA.toLowerCase() ? -1 : 1);
-    };
-    
-    if (!sortIDCol == true) {
-      return cat.sort((a, b) => a.id < b.id ? -1 : 1);
+      return cat.sort(function (a, b) {
+        if (a.nameUA.toLowerCase() < b.nameUA.toLowerCase()) {
+          return -1
+        }
+      })
     }
-    else return cat.reverse();
+
+    if (sortIDCol == undefined) {
+      return cat.sort(function (a, b) {
+        if (a.id < b.id) {
+          return -1
+        }
+      })
+    }
+
+    else {
+      return cat.reverse()
+    }
+
   }
 
 }
 
+
+// if (sortENCol == true) {
+//   return cat.sort((a, b) => a.nameEN.toLowerCase() < b.nameEN.toLowerCase() ? -1 : 1);
+// };
+
+// if (sortUACol == true) {
+//   return cat.sort((a, b) => a.nameUA.toLowerCase() < b.nameUA.toLowerCase() ? -1 : 1);
+// };
+
+// if (!sortIDCol == true) {
+//   return cat.sort((a, b) => a.id < b.id ? -1 : 1);
+// }
+// else return cat.reverse();
